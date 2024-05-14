480c33adf03d7499c01b1b572c815301a49f4456
Rizka17 <105841111421@student.unismuh.ac.id>
2024-05-14 15:08:30 +0800

    lab 1

--- App.js
+++ App.js
@@ -4,8 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>info magang abangku</Text>
+      <Text style={{
+        fontSize: 20,
+        fontStyle: 'italic',
+        color: 'black',
+        textDecorationLine: 'underline',
+        textShadowColor: 'red'
+      }}>info magang abangku</Text>
+      <Text style={{
+        fontSize:50,
+        fontWeight: 'bold',
+        color: 'pink'
+      }}>info magang abangku 50! </Text>
+      
+      
+      
       <StatusBar style="auto" />
+      
     </View>
   );
 }
