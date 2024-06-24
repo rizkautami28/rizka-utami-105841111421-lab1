fef0dddba0a80d0ca2366f201da841b3ecae93be
Rizka17 <105841111421@student.unismuh.ac.id>
2024-06-24 14:42:31 +0800

    Lab2

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import 'react-native';
 const App = () => {
   return (
-    <View style= {{flex: 1}}>
-      <View style= {{flex: 1,flexDirection: 'row'}}>
-   <View style= {{flex: 1,backgroundColor:'green'}}></View>
-   <View style= {{flex: 1,backgroundColor:'yellow'}}></View>
-    </View>
-    <View style= {{flex: 1,flexDirection: 'colum'}}>
-   <View style= {{flex: 1,backgroundColor:'red'}}></View>
-   <View style= {{flex: 1,backgroundColor:'blue'}}></View>
-    </View>
 
+    <View style = {{
+      flex: 1, flexDirection:'column',}}>
+      <View style={{
+         flex:1, backgroundColor: 'purple', }}></View>
+     <View style={{
+      flex: 1, backgroundColor: 'green', }}></View>
+      
+
+      <View style={{ flex: 1, flexDirection: 'row'}}>
+     <View style={{flex:1, backgroundColor: 'blue', }}></View>
     </View>
-  );
+     </View>
+  )
 }
+
 export default App
